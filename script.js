15cdecb8c4b32b939279bec368da70c22d8c4f9d
beingshahbaj <130038256+beingshahbaj@users.noreply.github.com>
2023-11-10 19:20:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const extendHex = (shortHex) => {
-  // write your code here
-};
+function extendHex(shortHex) {
+ 
+ if (shortHex.charAt(0) === '#') {
+    shortHex = shortHex.substring(1);
+ }
+
+ 
+ let hex = '';
+ for (let i = 0; i < 3; i++) {
+    let char = shortHex.charAt(i);
+    hex += (char.length === 1) ? char + char : char;
+ }
+
+
+ return '#' + hex;
+}
 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
